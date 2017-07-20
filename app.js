675ff8839be33c490ad068d64f35687df59693e0
andy lai <andy51002000@gmail.com>
2017-07-20 01:20:25 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+
+var AWS = require("aws-sdk");
+var iotdata = new AWS.IotData({endpoint: ''});
+
+exports.handler = function index(event, context, callback) {
+
+            var clientId = getDeviceByUser(event.session.user.userId)
+            var params = {
+                topic: clientId+'/monitor',
+                payload:  JSON.stringify('off'),
+                qos: 0
+                };
+                
+         
+            iotdata.publish(params, function(err, data){
+                if(err){
+                    console.log('err:'+err);
+                     context.fail(event); 
+                }
+                else{
+                    console.log("success?");
+                    context.succeed(event);
+                }
+ 
+            });
+                            
+            context.succeed(
+                  generateResponse(
+                    buildSpeechletResponse(`Okay i trying to turn off`, true),
+                    {}
+                  )
+                )
+            break;  
+
+}
+
